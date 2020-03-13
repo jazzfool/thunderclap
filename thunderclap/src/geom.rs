7e11b25db19b58907e82223dd76b47b3ffa7f694
jazzfool <saveuselon@gmail.com>
2020-03-13 22:28:17 +1100

    Document crate::geom module + boilerplate derives

--- thunderclap/src/geom.rs
+++ thunderclap/src/geom.rs
@@ -3,6 +3,7 @@
 use crate::base;
 
 /// Unit of absolute widget space.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct AbsoluteUnit;
 
 /// Point relative to the window instead of parent.
@@ -11,6 +12,7 @@ pub type AbsolutePoint = reclutch::euclid::Point2D<f32, AbsoluteUnit>;
 pub type AbsoluteRect = reclutch::euclid::Rect<f32, AbsoluteUnit>;
 
 /// Unit of relative widget space.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct RelativeUnit;
 
 /// Point relative to the parent.
@@ -18,11 +20,14 @@ pub type RelativePoint = reclutch::euclid::Point2D<f32, RelativeUnit>;
 /// Rectangle relative to the parent.
 pub type RelativeRect = reclutch::euclid::Rect<f32, RelativeUnit>;
 
+/// Getter/setter for widgets which store their parent's position.
 pub trait StoresParentPosition {
     fn set_parent_position(&mut self, parent_pos: AbsolutePoint);
     fn parent_position(&self) -> AbsolutePoint;
 }
 
+/// A point that can be either relative or absolute.
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum AgnosticPoint {
     Relative(RelativePoint),
     Absolute(AbsolutePoint),
@@ -40,6 +45,8 @@ impl From<AbsolutePoint> for AgnosticPoint {
     }
 }
 
+/// A rectangle that can be either relative or absolute.
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum AgnosticRect {
     Relative(RelativeRect),
     Absolute(AbsoluteRect),
@@ -57,24 +64,30 @@ impl From<AbsoluteRect> for AgnosticRect {
     }
 }
 
+/// Getters/setters for context-aware 2D translation.
 pub trait ContextuallyMovable: base::Movable + StoresParentPosition {
+    /// Changes the position to an agnostic point (i.e. accepts relative or absolute points).
     fn set_ctxt_position(&mut self, position: AgnosticPoint);
 
+    /// Returns the position relative to the window.
     #[inline]
     fn abs_position(&self) -> AbsolutePoint {
         self.position().cast_unit() + self.parent_position().to_vector()
     }
 
+    /// Returns the bounds with the position relative to the window.
     #[inline]
     fn abs_bounds(&self) -> AbsoluteRect {
         self.bounds().cast_unit().translate(self.parent_position().to_vector())
     }
 
+    /// Converts a point relative to this widget to an absolute point (relative to the window).
     #[inline]
     fn abs_convert_pt(&self, pt: RelativePoint) -> AbsolutePoint {
         pt.cast_unit() + self.parent_position().to_vector()
     }
 
+    /// Converts an absolute point (relative to the window) to a point relative to this widget.
     #[inline]
     fn rel_convert_pt(&self, pt: AbsolutePoint) -> RelativePoint {
         pt.cast_unit() - self.parent_position().to_vector().cast_unit()
@@ -94,20 +107,25 @@ impl<W: base::WidgetChildren> ContextuallyMovable for W {
     }
 }
 
+/// Getters/setters for context-aware 2D rectangle translation.
 pub trait ContextuallyRectangular: ContextuallyMovable + base::Rectangular {
+    /// Changes the rectangle to an agnostic rectangle (i.e. accepts relative and absolute coordinates).
     fn set_ctxt_rect(&mut self, rect: impl Into<AgnosticRect>);
 
+    /// Returns the relative rectangle in absolute coordinates (i.e. relative to the window).
     #[inline]
     fn abs_rect(&self) -> AbsoluteRect {
         self.rect().cast_unit().translate(self.parent_position().to_vector())
     }
 
+    /// Converts a rectangle relative to this widget into absolute coordinates (i.e. relative to the window).
     #[inline]
     fn abs_convert_rect(&self, mut rect: RelativeRect) -> AbsoluteRect {
         rect.origin = self.abs_convert_pt(rect.origin).cast_unit();
         rect.cast_unit()
     }
 
+    /// Converts an absolute rectangle (i.e. relative to the window) into relative coordinates.
     #[inline]
     fn rel_convert_rect(&self, mut rect: AbsoluteRect) -> RelativeRect {
         rect.origin = self.rel_convert_pt(rect.origin).cast_unit();
